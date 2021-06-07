d42a72a2e47bd5e439bd5e749be6abfaaea19ab9
carlsondultra <carlsondultra@users.noreply.github.com>
2021-06-06 21:41:49 -0400

    Creating variables for javascript cards

---
+++ memory.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    var cards = ['shoe', 'bred', 'dior', 'mocha', 'blackwhite', 'universityblue', 'sbb', 'shadow']; // Represents each pair of cards
+    var pairs = cards.concat(cards);//create pairs of cards
+    var chosenCards = [];
+    var cardsToFlip = [];
+    
+    var gameStarted = false;
+    var running = false;
+    var outOfTime = false;
+    var countdownStarted = false;
+    var win = false;
+    var pairCount = 0;
+    var time = 60;
+});
