e2e9cc5c2c88aa0e3bc49ca5355c14a1e25b3cce
carlsondultra <carlsondultra@users.noreply.github.com>
2021-06-08 23:32:51 -0400

    Adding ability to shuffle the card orders

--- memory.js
+++ memory.js
@@ -129,5 +129,14 @@ $(document).ready(function() {
         
     });//Flip Container On Click Finished
     
+    function shuffleArray(array) { //Shuffling cards
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
     
 });
